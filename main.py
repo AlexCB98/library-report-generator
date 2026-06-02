7686aef7b360d8f48e8b1d452a998191bab078be
Alexandru <lexyus22@gmail.com>
2026-06-02 15:21:02 +0200

    Generate individual book files

--- main.py
+++ main.py
@@ -13,3 +13,6 @@ with open('Output/library_report.txt', mode = 'w') as write_report:
         clear_book_name = book.strip()
         write_report.write(f'{counter}. {clear_book_name}\n')
         counter += 1
+
+        with open(f'Output/{clear_book_name}.txt', mode='w') as completed_files:
+            completed_files.write(f'Book: {clear_book_name}\nStatus: Available')
